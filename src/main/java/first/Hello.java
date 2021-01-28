d276812a20d696f53de4142e853c029a05fe655f
Lun-learn <aa455931143@gmail.com>
2021-01-28 15:30:02 +0800

    Java and Kotlin variable,data type

--- src/main/java/first/Hello.java
+++ src/main/java/first/Hello.java
@@ -5,6 +5,15 @@ public class Hello {
         //System.out.println("Hello world");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        float weight = 66.5f;
+        boolean enroll = false;
+        String name = "Tom";
+
+        Integer age2 = 19;
+
+
+
     }
 
 }
--- src/main/kotlin/first/Hello.kt
+++ src/main/kotlin/first/Hello.kt
@@ -4,6 +4,13 @@ fun main() {
     //println("Hello world")
     val h = Human()
     h.hello()
+
+    var weight = 66.5
+    var name : String
+    name = "Hank"
+
+    var age = 19;
+    age = 20;
 }
 
 class Human{
